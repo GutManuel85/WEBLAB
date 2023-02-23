30c60527fdde4b8ade0911bf665198f4ebe9a43f
GutManuel85 <gutmanuel@gmx.ch>
2023-02-23 05:57:08 +0100

    First version of Student Area

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import {
 } from "./admin-area/students-admin/subject-student-mapping/subject-student-mapping.component";
 import {LogInComponent} from "./log-in/log-in.component";
 import {SubjectGradesComponent} from "./admin-area/subjects-admin/subject-grades/subject-grades.component";
+import {StudentAreaComponent} from "./student-area/student-area.component";
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent},
@@ -21,6 +22,7 @@ const routes: Routes = [
   { path: 'subjects-admin/grades/:id', component: SubjectGradesComponent},
   { path: 'classes-admin', component: ClassesAdminComponent},
   { path: 'teacher', component: TeacherAreaComponent},
+  { path: 'student-area', component: StudentAreaComponent},
 ];
 
 @NgModule({
--- app/src/app/student-area/student-area.component.ts
+++ app/src/app/student-area/student-area.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../services/student.service';
+import {SubjectService} from "../services/subject.service";
 
 @Component({
-  selector: 'app-student-area',
+  selector: 'app-teacher-area',
   templateUrl: './student-area.component.html',
   styleUrls: ['./student-area.component.css']
 })
-export class StudentAreaComponent {
+export class StudentAreaComponent implements OnInit {
+
+  students: any[];
+  subjects: any[]; // Add this property for the subject list
+
+  constructor(private studentService: StudentService, private subjectService: SubjectService) { }
+
+  ngOnInit(): void {
+    this.studentService.getStudents().subscribe((students) => {
+      this.students = students;
+    });
+
+    this.subjectService.getSubjects().subscribe((subjects) => {
+      this.subjects = subjects;
+    });
+  }
+
+  getSubjectName(subjectId: string): string {
+    const subject = this.subjects.find((s) => s.id === subjectId);
+    return subject ? subject.name : '';
+  }
 
 }
